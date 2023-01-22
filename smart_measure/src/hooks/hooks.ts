247ccc64b1319a5b0480c6a6ea85a08949140b55
Yahia <yahia.jabeur@gmail.com>
2023-01-22 12:51:32 +0100

    add smart measure react native app

---
+++ smart_measure/src/hooks/hooks.ts
@@ -0,0 +1,41 @@
+import Geolocation from '@react-native-community/geolocation';
+import {useEffect, useMemo, useState} from 'react';
+import {LatLng} from 'react-native-maps/lib/sharedTypes';
+
+export const useStoresPosition = () => {
+  const [currentPosition, setCurrentPosition] = useState<LatLng | undefined>();
+  const [authorized, setAuthorized] = useState(false);
+
+  const initialRegion = useMemo(() => {
+    if (currentPosition) {
+      return {
+        latitude: currentPosition?.latitude,
+        longitude: currentPosition?.longitude,
+        latitudeDelta: 0.0005,
+        longitudeDelta: 0.0005,
+      };
+    }
+  }, [currentPosition]);
+
+  useEffect(() => {
+    Geolocation.requestAuthorization(
+      () => setAuthorized(true),
+      error => console.info('Geolocation permission error', error.message),
+    );
+
+    Geolocation.getCurrentPosition(
+      position =>
+        setCurrentPosition({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+        }),
+      err => console.info('GetCurrentPosition error', err.message),
+      {enableHighAccuracy: false, timeout: 5000, maximumAge: 10000},
+    );
+  }, [authorized]);
+
+  return {
+    initialRegion,
+    currentPosition,
+  };
+};
